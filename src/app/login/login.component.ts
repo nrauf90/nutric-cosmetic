9eb56519dbcc3ea9f842abb3ab06f47e49cf8099
Muhammad Noman Rauf <mrau@ciklum.com>
2021-10-05 18:11:21 +0500

    Fix form validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,20 +8,80 @@ import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 })
 export class LoginComponent implements OnInit {
 
-  public loginForm: any;
+  /*
+  * Form Group to bind input controls
+  */
+  loginForm = new FormGroup({
+    email: new FormControl('',[Validators.email, Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
+
+  /*
+  * Validation variables
+  */
+  emailError = false;
+  emailRMsg = '';
+  passwordError = false;
+  passwordMsg = '';
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
 
-    this.loginForm = new FormGroup({
-      email: new FormControl(this.loginForm.email,[Validators.email, Validators.required]),
-      password: new FormControl(this.loginForm.password, [Validators.required, Validators.min(1), Validators.maxLength(6)])
-    })
   }
 
+  /*
+  * Login function to validate and submit data
+  */
   login() {
-    console.log();
+
+    /*
+    * Validate input data before login
+    */
+    if(this.loginValidation()){
+      console.log(this.loginForm.value);
+    }
+
   }
 
+  /*
+  * Form validation function
+  */
+  loginValidation(){
+    /*
+    * Email validation before login
+    */
+    if(this.loginForm.get('email')?.hasError('required')){
+      this.emailError = true;
+      this.emailRMsg = 'Email is required.'
+    }else{
+      if(this.loginForm.get('email')?.hasError('email')){
+        this.emailError = true;
+        this.emailRMsg = 'Please enter valid email.'
+      }else{
+        this.emailError = false
+      }
+    }
+
+    /*
+    * Password validation before login
+    */
+    if(this.loginForm.get('password')?.hasError('required')){
+      this.passwordError = true;
+      this.passwordMsg = 'Password is required.'
+    }else{
+      if(this.loginForm.get('password')?.hasError('minlength')){
+        this.passwordError = true;
+        this.passwordMsg = 'Password must be at least 6 character long.'
+      }else{
+        this.passwordError = false
+      }
+    }
+
+    if( this.emailError || this.passwordError){
+      return false;
+    }else{
+      return true;
+    }
+  }
 }
