2a8a2817f1e00665895a474043dc6262b90f6862
Muhammad Noman Rauf <mrau@ciklum.com>
2021-10-05 14:35:16 +0500

    Login changes.html

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,27 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
-import {ErrorStateMatcher} from '@angular/material/core';
-
-/** Error when invalid control is dirty, touched, or submitted. */
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
-}
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
-  emailFormControl = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
+  constructor() { }
 
-  matcher = new MyErrorStateMatcher();
+  ngOnInit(): void {
+  }
 
 }
