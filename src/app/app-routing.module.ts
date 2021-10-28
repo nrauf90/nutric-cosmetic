c43e9e86218e32dad685b9f9b9d9c8494274d7de
naim-sajjad <naimsajjad15@gmail.com>
2021-10-28 13:06:05 +0500

    resolve minor design issues in all pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,21 +4,21 @@ import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
 import {BillingComponent} from "./billing/billing.component";
 import {CompanyComponent} from "./company/company.component";
+import {ProductGridComponent} from "./productGrid/productGrid.component";
 import {ProductInfoComponent} from "./productInfo/productInfo.component";
 import {ProductsComponent} from "./products/products.component";
 import {SyncingComponent} from "./syncing/syncing.component";
-import {ProductGridComponent} from "./productGrid/productGrid.component";
 
 const routes: Routes = [
-{ path: '', component: LoginComponent },
-{ path: 'register', component: RegisterComponent },
-{ path: 'billing', component: BillingComponent },
-{ path: 'company', component: CompanyComponent },
-{ path: 'product-detail', component: ProductGridComponent },  
-{ path: 'product-info', component: ProductInfoComponent },
-{ path: 'products', component: ProductsComponent },
-{ path: 'syncing', component: SyncingComponent },
-{ path: '**', component: LoginComponent }
+  { path: '', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'billing', component: BillingComponent },
+  { path: 'company', component: CompanyComponent },
+  { path: 'product-detail', component: ProductGridComponent },  
+  { path: 'product-info', component: ProductInfoComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'syncing', component: SyncingComponent },
+  { path: '**', component: LoginComponent }
 ];
 
 @NgModule({
---
+++ src/app/productdetail/productdetail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './productdetail.component.html',
+  styleUrls: ['./productdetail.component.scss']
+})
+export class ProductdetailComponent implements OnInit {
+
+
+  ngOnInit(): void {
+
+  }
+
+}
